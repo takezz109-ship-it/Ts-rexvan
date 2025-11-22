1c382df8c36896be438d4bee791b74c5831ca2ec
takezz109-ship-it <takezz109@gmail.com>
2025-11-22 14:18:09 +0100

    Add files via upload

---
+++ guests.py.py
@@ -0,0 +1,39 @@
+#student name: TSE REXVAN
+#department: SOFTWARE  ENGINEERING
+
+guest_list = ["frank, rexvan, albert, junior"]
+ 
+print("frank im inviting you to dinner")
+print("rexvan im inviting you to dinner")
+print("albert im inviting you to dinner")
+print("junior im inviting you to dinner")
+
+guest_list = ["frank, rexvan, albert, junior"]
+
+print("frank cant make it for dinner")
+
+guest_list.pop(0)
+
+print(guest_list)
+
+guest_list.insert(0, "chatto")
+
+print("chatto im inviting you to dinner")
+print("rexvan im inviting you to dinner")
+print("albert im inviting you to dinner")
+print("junior im inviting you to dinner")
+
+guest_list = ["chatto, rexvan, albert, junior"]
+
+print("hello everyone i have found a bigger dinner table")
+
+guest_list.insert(0, "cosmos")
+guest_list.insert(2, "theodore")
+guest_list.append("franklin")
+
+print("cosmos im inviting you to dinner")
+print("rexvan im inviting you for dinner")
+print("theodore iminviting you for dinner")
+print("albert im inviting you for dinner")
+print("junior im inviting you for dinner")
+print("franklin im inviting you for dinner")
---
+++ visit,py.py
@@ -0,0 +1,24 @@
+#student name: Tse Rexvan
+#department: software engineering
+
+places=["france, egypt, America,dubai"]
+print(places) 
+
+result = sorted(places)
+print(result)
+print(places) 
+places=["france, egypt, america, dubai"]
+places.reverse()
+places=["france, egypt, america, dubai"]
+rev = places[::-1]
+original = rev[::-1]
+print(places)
+
+places=["london, egypt, america, dubai"]
+places.sort()
+print(places) 
+
+places=["france, egypt, america, america "]
+places.sort(reverse ="london" )
+print(places)
+
